e11a6b56eff1ae059a60399c55f52d96a853dfa1
Alex Johnson <alexwj@gmx.com>
2016-02-07 04:26:04 -0500

    Storage has been upgraded

--- Chrome Extension/alert.js
+++ Chrome Extension/alert.js
@@ -1,1 +1,1 @@
-alert('Yooo');
+alert("hi");
--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -1,7 +1,7 @@
 function hello(){
-	chrome.tabs.executeScript({
-		file: 'alert.js'
-	});
+	var input = document.getElementById('in').value;
+	var key = document.getElementById('key').value;
+	confirm(key);
 }
 
 document.getElementById('helen').addEventListener('click', hello);
