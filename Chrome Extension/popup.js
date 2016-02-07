3a152ef7b93b19c257ecc77b3430a9ccc0c9fa94
Alex Johnson <alexwj@gmx.com>
2016-02-07 05:44:04 -0500

    Initial version of the code behind the one time pad

--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -2,7 +2,11 @@ function hello(){
 	var input = document.getElementById('in').value;
 	var key = document.getElementById('key').value;
 	if(input.length == key.length){
-		chrome.tabs.executeScript(null,{code:"alert('yo')"});
+		var out = ''
+		for(var i = 0; i < input.length; i++){
+			out += String.fromCharCode(input.charCodeAt(i) ^ key.charCodeAt(i));
+		}
+		alert(out);
 	}
 	else{
 		chrome.tabs.executeScript(null,{code:"alert('The length of the input and key need to be the same!')"});
