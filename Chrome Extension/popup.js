aa5c9c6c94667b403b2cf8e5e2a139b552617b79
Alex Johnson <alexwj@gmx.com>
2016-02-07 04:56:03 -0500

    Poppet kinda works

--- Chrome Extension/popup.js
+++ Chrome Extension/popup.js
@@ -1,7 +1,12 @@
 function hello(){
 	var input = document.getElementById('in').value;
 	var key = document.getElementById('key').value;
-	confirm(key);
+	if(input.length == key.length){
+		chrome.tabs.executeScript(null,{code:"alert('yo')"});
+	}
+	else{
+		chrome.tabs.executeScript(null,{code:"alert('The length of the input and key need to be the same!')"});
+	}
 }
 
 document.getElementById('helen').addEventListener('click', hello);
